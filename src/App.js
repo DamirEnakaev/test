b8da1f3dffc95e79b61dc2c05dae88ae99a9a73a
Damir <enakaev@gmail.com>
2022-11-28 00:26:52 +0200

    adding a button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { Helmet } from 'react-helmet';
+
 
 function App() {
 
-const TITLE = 'GAY TEST'
+
 
   return (
     <div className="App">
-      <Helmet>
-      <title>{TITLE}</title>
-      </Helmet>
+
       This test is supposed to help you to find yourself
+
+      
+      <button>Start test</button>
+     
 
     </div>
   );
